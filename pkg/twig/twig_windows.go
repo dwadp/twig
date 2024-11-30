d272ed3be426c992085977682d25b1f84ba42765
Adi Perdana <dewaadiperdana@gmail.com>
2024-11-30 15:12:12 +0800

    remove TTY on windows as it's unecessary

--- pkg/twig/twig_windows.go
+++ pkg/twig/twig_windows.go
@@ -4,41 +4,18 @@
 package twig
 
 import (
-	"io"
 	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
-
-	"github.com/creack/pty"
-	"golang.org/x/term"
 )
 
 func runInTTY(cmd *exec.Cmd) error {
-	ptmx, err := pty.Start(cmd)
-	if err != nil {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
 		return err
 	}
-
-	defer func() { _ = ptmx.Close() }()
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		sig := <-sigCh
-		_ = cmd.Process.Signal(sig)
-	}()
-
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = term.Restore(int(os.Stdin.Fd()), oldState)
-	}()
-
-	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
-	_, _ = io.Copy(os.Stdout, ptmx)
 
 	return nil
 }
