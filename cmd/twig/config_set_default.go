bbdb5e8a959ace20c5d30c552b58dd36e663b7ae
Adi Perdana <dewaadiperdana@gmail.com>
2024-11-30 18:43:33 +0800

    implements config set-default command

--- cmd/twig/config_set_default.go
+++ cmd/twig/config_set_default.go
@@ -1,12 +1,20 @@
 package twig
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var configSetDefaultCmd = &cobra.Command{
-	Use: "set-default",
+	Use:  "set-default",
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.PrintErrln("Not implemented yet")
+		if err := cfg.SetDefault(args[0]); err != nil {
+			cmd.PrintErr(err)
+			os.Exit(1)
+		}
+
+		cmd.Printf("default PHP version set to %s\n", args[0])
 	},
 }
